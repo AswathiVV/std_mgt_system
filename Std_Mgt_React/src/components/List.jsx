bbf1d00010f7bc0d664b543a562684761d5bec4f
AswathiVV <aswathivv916@gmail.com>
2025-01-13 13:37:56 +0530

    std_mgt_stm

---
+++ Std_Mgt_React/src/components/List.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+function List(){
+    const [data,setData] = useState([])
+    const [editing,setEditing] = useState(false)
+    const [editdata,setEditData] =useState(null)
+    useEffect(()=>{
+        axios.get('http://127.0.0.1:8000/api/std/').then((res)=>{
+            console.log(res.data);
+            setData(res.data)
+        }).catch(error=>console.log(error.message))
+        
+    },[])
+    const Edit_dtls =(task) =>{
+        setEditing(true)
+        setEditData(task)
+    }
+
+    const updateDtls =(id,task) =>{
+        setEditing(false)
+        axios.put(`http://127.0.0.1:8000/api/std/${id}/`,task).then(res=>{
+            setData(data.map((prv)=>prv.id===id ? res.data : prv))
+        }).catch(error=>console.log(error.message))
+    }
+    return(
+        <div className="container">
+            <h1>Student Details</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Roll no</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>email</th>
+                        <th>Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((value,index)=>(
+                        <tr key={index}>
+                            <td>{value.roll_no}</td>
+                            <td>{value.name}</td>
+                            <td>{value.age}</td>
+                            <td>{value.email}</td>
+                            <td>{value.phno}</td>
+                            <td><button className="btn btn-outline-info" onClick={()=>{Edit_dtls(value)}}>Edit</button></td>
+                            <td><button className="btn btn-outline-danger" onClick={()=>{}}><i className="bi bi-trash3"></i></button></td>
+
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            {editing ? <EditForm  curTask={editdata} updatefun={updateDtls}/>:null}
+        </div>
+    )
+}
+
+const EditForm = ({curTask,updatefun})=>{
+    const [task,setTask] =useState(curTask)
+
+    const handleChange = (e) =>{
+        const{name,value} = e.target
+        setTask({...task,[name]:value})
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        updatefun(task.id,task)
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="number" name="roll_no" id="roll_no" value={task.roll_no} onChange={handleChange}/>
+            <input type="text" name="name" id="name" value={task.name} onChange={handleChange}/>
+            <input type="number" name="age" id="age" value={task.age} onChange={handleChange}/>
+            <input type="text" name="email" id="email" value={task.email} onChange={handleChange}/>
+            <input type="text" name="phno" id="phno" value={task.phno} onChange={handleChange}/>
+            <input type="submit" value="update"/>
+        </form>
+    )
+}
+
+export default List
